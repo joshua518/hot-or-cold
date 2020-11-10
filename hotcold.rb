067fff68a8a769d3c862b25593789b7420aefc0c
Joshua Dale Parelanan <mpadrelanan@gmail.com>
2020-11-10 21:01:51 +0800

    Added hot or cold logic

--- hotcold.rb
+++ hotcold.rb
@@ -6,6 +6,16 @@ loop do
 
   if target_number > number 
     puts "My number is higher!"
+    distance = (target_number - number).abs 
+    if distance <= 5
+      puts "Ayo it's very hot man."
+    elsif distance <= 10 
+      puts "My dude it's hot."
+    elsif distance <= 20
+      puts "It's cold brah."
+    else 
+      puts "Oi mate, it's very cold. Turn on the heater."
+    end
   elsif target_number < number 
     puts "My number is lower!"
   else 
