297e4436580ce7166dc3160e9f923a939b13c655
Joshua Dale Parelanan <mpadrelanan@gmail.com>
2020-11-10 21:18:13 +0800

    Fixed bugs

--- hotcold.rb
+++ hotcold.rb
@@ -3,10 +3,10 @@ target_number = rand(1..100)
 loop do
   print "What is my number? "
   number = Integer(gets)
+    distance = (target_number - number).abs 
 
   if target_number > number 
     puts "My number is higher!"
-    distance = (target_number - number).abs 
     if distance <= 5
       puts "Ayo it's very hot man."
     elsif distance <= 10 
@@ -17,6 +17,15 @@ loop do
       puts "Oi mate, it's very cold. Turn on the heater."
     end
   elsif target_number < number 
+    if distance <= 5
+      puts "Ayo it's very hot man."
+    elsif distance <= 10 
+      puts "My dude it's hot."
+    elsif distance <= 20
+      puts "It's cold brah."
+    else 
+      puts "Oi mate, it's very cold. Turn on the heater."
+    end
     puts "My number is lower!"
   else 
     puts "Yay! You got the right number. My number is #{target_number}."
